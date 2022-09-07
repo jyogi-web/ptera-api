8094000ed74e00b4a706fc4c4f715bd500554287
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-09-08 00:30:28 +0900

    init

---
+++ src/main.rs
@@ -0,0 +1,97 @@
+use aws_sdk_dynamodb::{model::AttributeValue, Client};
+use lambda_http::{
+    http::{request, Method},
+    request::RequestContext,
+    run, service_fn, Body, Error, Request, RequestExt, Response,
+};
+use serde::{Deserialize, Serialize};
+use serde_json::json;
+
+#[derive(Debug, Default, Serialize, Deserialize)]
+struct RateInfo {
+    uuid: String,
+    rate: u64,
+    name: String,
+}
+
+/// This is the main body for the function.
+/// Write your code inside it.
+/// There are some code example in the following URLs:
+/// - https://github.com/awslabs/aws-lambda-rust-runtime/tree/main/examples
+async fn function_handler(event: Request) -> Result<Response<Body>, Error> {
+    let region = std::env::var("region").unwrap_or_default();
+
+    let shared_config = aws_config::load_from_env().await;
+    let client = Client::new(&shared_config);
+    let req = client.list_tables().limit(10);
+    let list_tables = req.send().await;
+    let list_tables = match list_tables {
+        Ok(l) => l.table_names.unwrap()[0].clone(),
+        Err(e) => e.to_string(),
+    };
+    let request = client
+        .put_item()
+        .table_name(&list_tables)
+        .item(
+            "user_id",
+            AttributeValue::S(uuid::Uuid::new_v4().to_string()),
+        )
+        .item("name", AttributeValue::S("kona".to_string()))
+        .item("rate", AttributeValue::N(4440.to_string()));
+    request.send().await.unwrap();
+
+    let item = client.get_item().table_name(&list_tables).key(
+        "user_id",
+        AttributeValue::S("99315bb2-c1eb-4875-9080-67f41281ea7c".to_string()),
+    );
+    let item = item.send().await?;
+    dbg!(item.item);
+    let name = "";
+
+    // Extract some useful information from the request
+    let resource_path = if let RequestContext::ApiGatewayV2(http_context) = event.request_context()
+    {
+        http_context.http.path.unwrap()
+    } else {
+        unreachable!()
+    };
+
+    match (event.method(), resource_path.as_ref()) {
+        (&Method::GET, "/dev/rate") => {}
+        _ => (),
+    }
+
+    // Return something that implements IntoResponse.
+    // It will be serialized to the right response event automatically by the runtime
+    let resp = Response::builder()
+        .status(200)
+        .header("content-type", "application/json")
+        .body(
+            json!({
+                "message": "This is a Rust server!",
+                "method": event.method().as_ref(),
+                "path_param": event.path_parameters(),
+                "resource_path": resource_path,
+                "tables": list_tables,
+                "region": region,
+                "name": name
+            })
+            .to_string()
+            .into(),
+        )
+        .map_err(Box::new)?;
+    Ok(resp)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::INFO)
+        // disable printing the name of the module in every log line.
+        .with_target(false)
+        // disabling time is handy because CloudWatch will add the ingestion time.
+        .without_time()
+        .init();
+
+    run(service_fn(function_handler)).await
+}
