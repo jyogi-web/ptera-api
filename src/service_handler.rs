c01bbf5e3d303691a5331650cdb640a051cc41d8
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-09-19 02:40:13 +0900

    fix: if no content, return 204

--- src/service_handler.rs
+++ src/service_handler.rs
@@ -14,14 +14,22 @@ const DEFAULT_RATE: u64 = 0;
 pub async fn get_rate_handler(event: &Request) -> Result<Response<Body>> {
     let id = event.headers().get("ptera-id").unwrap().to_str().unwrap();
 
-    let rate_info = get_rate(id).await.unwrap_or_default();
-
-    let resp = Response::builder()
-        .status(200)
-        .header("content-type", "application/json")
-        .body(json!(rate_info).to_string().into())
-        // .body(json!({"message":"GET /rate", "id": id}).to_string().into())
-        .context("Failed to Response body.")?;
+    let rate_info = get_rate(id).await;
+    let resp = match rate_info {
+        Ok(rate_info) => Response::builder()
+            .status(200)
+            .header("content-type", "application/json")
+            .body(json!(rate_info).to_string().into())
+            .context("Failed to Response body.")?,
+        Err(e) => {
+            log::debug!("{}", &e);
+            Response::builder()
+                .status(204)
+                .header("content-type", "application/json")
+                .body(json!(RateInfo::default()).to_string().into())
+                .context("Failed to Response body.")?
+        }
+    };
 
     Ok(resp)
 }
@@ -29,12 +37,14 @@ pub async fn get_rate_handler(event: &Request) -> Result<Response<Body>> {
 pub async fn post_rate_handler(event: &Request) -> Result<Response<Body>> {
     let insert_rate_info =
         String::from_utf8(event.body().to_vec()).context("Failed to vec to String")?;
+    log::debug!("{}", &insert_rate_info);
     let mut insert_rate_info: RateInfo =
         serde_json::from_str(&insert_rate_info).context("Failed to str to json")?;
     insert_rate_info.rate = DEFAULT_RATE;
     log::debug!("[Body] {:#?}", insert_rate_info);
 
     insert_rate(&insert_rate_info).await?;
+    log::info!("Inserted rate");
 
     let resp = Response::builder()
         .status(200)
