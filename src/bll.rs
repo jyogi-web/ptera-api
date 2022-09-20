6165dbf17ff1c1c1fad6d41f3b53c3dfe1ccf301
Romira915 <40430090+Romira915@users.noreply.github.com>
2022-09-21 03:48:20 +0900

    fix: log_2{0}を回避

--- src/bll.rs
+++ src/bll.rs
@@ -5,8 +5,8 @@ pub(crate) fn rate_calculation(win: u64, lose: u64) -> (u64, u64) {
     let lose = lose as f64;
     let (update_win, update_lose) = if win >= lose {
         // 格上が勝った場合
-        let delta_win = ((lose.log2() + 50.0) as u64).min(CONFIG.max_delta_rate);
-        let delta_lose = ((win.log2() + 20.0) as u64).min(CONFIG.max_delta_rate);
+        let delta_win = (((lose + 1.).log2() + 50.0) as u64).min(CONFIG.max_delta_rate);
+        let delta_lose = (((win + 1.).log2() + 20.0) as u64).min(CONFIG.max_delta_rate);
 
         let win = win as u64;
         let lose = lose as u64;
@@ -16,9 +16,11 @@ pub(crate) fn rate_calculation(win: u64, lose: u64) -> (u64, u64) {
         )
     } else {
         // 格下が勝った場合
-        let delta_win = ((lose.powi(4).log2() + (win - lose).abs().log2() + 50.0) as u64)
+        let delta_win = (((lose + 1.).powi(4).log2() + ((win - lose).abs() + 1.).log2() + 50.0)
+            as u64)
             .min(CONFIG.max_delta_rate);
-        let delta_lose = ((win.powi(2).log2() + (win - lose).abs().log2() + 20.0) as u64)
+        let delta_lose = (((win + 1.).powi(2).log2() + ((win - lose).abs() + 1.).log2() + 20.0)
+            as u64)
             .min(CONFIG.max_delta_rate);
 
         let win = win as u64;
